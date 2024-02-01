ac2c305e3cf3e5f96ac72d38ea752c0632cb0b14
jakob <jakobzden1@gmail.com>
2024-02-01 10:12:23 +0100

    Movieclass fixeed

---
+++ ImoviesRepetition/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImoviesRepetition
+{
+    public class Movie
+    {
+        private DateTime playTime;
+
+        public DateTime Playtime
+        {
+            set { playTime = value; }
+            get { return playTime; }
+        }
+
+        public string MovieTitle { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Duration { get; set; }
+        public string Instructor { get; set; }
+        
+        public int CinemaHall { get; set; }
+
+
+        public DateTime OpeningDate { get; set; }
+
+
+
+    }
+}
