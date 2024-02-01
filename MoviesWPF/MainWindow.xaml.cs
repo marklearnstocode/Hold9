19febc8f1ee38f5b3821376c7152b67d61b0a5ee
Patrick <patrickhass84@gmail.com>
2024-02-01 10:41:54 +0100

    Lavet textboxes og buttons

--- MoviesWPF/MainWindow.xaml.cs
+++ MoviesWPF/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace MoviesWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
