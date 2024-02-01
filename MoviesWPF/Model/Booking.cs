e266f92ce5ad256b092048ab4b43e75087b66cd7
Dan Jakobsen <daja59887@edu.ucl.dk>
2024-02-01 13:10:31 +0100

    Opdateret med felter

--- MoviesWPF/Model/Booking.cs
+++ MoviesWPF/Model/Booking.cs
@@ -8,13 +8,24 @@ namespace MoviesWPF.Model
 {
     public class Booking
     {
-        public string MovieTitle {  get; set; }
+        public string Cinema;
+        public string City;
+        public DateTime PlayTime;
+        public string MovieTitle;
+        public string MovieGenre;
+        public double MovieLength;
+        public string MovieInstructor;
+        public DateTime PremiereDate;
+        public string Email;
+        public int PhoneNumber;
+
+        public string MovieTitle { get; set; }
         public DateTime PlayTime { get; set; }
         public int CinemaHall { get; set; }
 
         public int TicketAmount { get; set; }
         public string Email { get; set; }
-        public int Phonenumber { get; set; }
+        public int PhoneNumber { get; set; }
 
     }
 }
